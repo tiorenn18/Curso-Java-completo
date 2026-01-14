c267e4e726141caf01a90ea907ceac347f9bf783
Tiorenn18 <ericorenanleal124@gmail.com>
2026-01-14 14:35:13 -0300

    Exercicio Exceptions - Soluçao (muito ruim)

---
+++ Exercicio_Exceptions/Program/program.java
@@ -0,0 +1,59 @@
+package Exercicio_Exceptions.Program;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import Exercicio_Exceptions.Model.Entites.Reservation;
+
+public class program {
+    public static void main(String[] args) throws ParseException {
+
+    Scanner sc = new Scanner(System.in);
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    System.out.print("Room Number: ");
+    Integer rommNumber = sc.nextInt();
+
+    System.out.print("Checkin Date (dd/MM/yyyy): ");
+    Date checkIn = sdf.parse(sc.next());
+    System.out.print("CheckOut Date (dd/MM/yyyy): ");
+    Date checkOut = sdf.parse(sc.next());
+
+    Reservation reservation = null;
+    if (! checkOut.after(checkIn)) {
+        System.out.println("Error reservation: check-out date must be after check-in date");
+    }
+    else {
+        reservation = new Reservation(rommNumber, checkIn, checkOut);
+        System.out.println("Reservation: " + reservation);
+    }
+
+    System.out.println();
+
+    System.out.println("Enter data to updated the reservation: ");
+
+    System.out.print("Checkin Date (dd/MM/yyyy): ");
+    checkIn = sdf.parse(sc.next());
+
+    System.out.print("CheckOut Date (dd/MM/yyyy): ");
+    checkOut = sdf.parse(sc.next());
+    reservation.updatedDates(checkIn, checkOut);
+
+    Date now = new Date();
+    if (checkIn.before(now) || checkOut.before(now)) {
+        System.out.println("Error in reservation: Reservation dates for uptade must be future.");
+    }
+    else if (!checkOut.after(checkIn)){
+        System.out.println("Error in reservation: Reservation dates for uptade must be future.");
+    }
+    else {
+        reservation.updatedDates(checkIn, checkOut);
+        System.out.println("Reservation: " + reservation);
+    }
+    
+    sc.close();
+    }
+
+}
