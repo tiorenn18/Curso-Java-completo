16a921771744b16ae439ac58da320498d707f5dd
Tiorenn <ericorenanleal124@gmail.com>
2026-02-26 18:37:35 -0300

    Melhorando fechamento de recursos na consulta da tabela department

--- recover_data/src/main/java/database/application/Program.java
+++ recover_data/src/main/java/database/application/Program.java
@@ -5,31 +5,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import database.classes.connectionFactory;
+import database.classes.ConnectionFactory;
 
 public class Program {
     public static void main(String[] args) {
         
-        Connection conn = null;
-        Statement st = null;
-        ResultSet rs = null;
-        
-        try {
-            conn = connectionFactory.connectDatabase();
-            st = conn.createStatement();
-            rs = st.executeQuery("select * from department");
+        try (Connection conn = ConnectionFactory.getConnection();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select * from department");
+        ) {
 
             while (rs.next()) {
-                System.out.printf(("Id: %d, Name: %s \n"), rs.getInt("Id"), rs.getString("Name"));
+                System.out.printf("Id: %d, Name: %s \n",
+                        rs.getInt("Id"), 
+                        rs.getString("Name"));
             }
         }  catch (SQLException e){
             System.out.println(e.getMessage());
-        }
-        finally{
-            connectionFactory.closeResultSet(rs);
-            connectionFactory.closeStatement(st);
-            connectionFactory.closeConnection(conn);
-            
+       
         }
     }
 }
--- recover_data/src/main/java/database/classes/connectionFactory.java
+++ recover_data/src/main/java/database/classes/connectionFactory.java
@@ -2,56 +2,15 @@ package database.classes;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
-public class connectionFactory {
+public class ConnectionFactory {
     
     private static final String URL = "jdbc:mysql://localhost:3306/coursedb";
     private static final String USER = "developer";
     private static final String USER_PASSWORD = "12345";
 
-    static Connection conn = null;
-
-    public static Connection connectDatabase() throws SQLException{
-        try {
-            if (conn == null) {
-                conn = DriverManager.getConnection(URL, USER, USER_PASSWORD);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return conn;
-    }
-
-    public static void closeConnection(Connection conn){
-        if (conn != null) {
-            try {
-                conn.close();
-            } catch (Exception e) {
-                System.out.println("Erro " + e.getMessage());
-            }
-        }
-    }
-
-    public static void closeStatement(Statement st){
-        if (st != null) {
-            try{
-                st.close();
-            } catch (SQLException e){
-                System.out.println(e.getMessage());
-            }
-        }
-    }
-
-    public static void closeResultSet(ResultSet rs){
-        if (rs != null) {
-            try{
-                rs.close();
-            } catch (SQLException e){
-                System.out.println(e.getMessage());
-            }
-        }
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(URL, USER, USER_PASSWORD);
     }
 }
