2534f347c3028de47d47127aa47c68a63faec204
Tiorenn <ericorenanleal124@gmail.com>
2026-05-11 16:37:43 -0300

    Carregando uma tela FXML

--- JavaFx/Aulas/aula2/src/main/java/curso/javafx/App.java
+++ JavaFx/Aulas/aula2/src/main/java/curso/javafx/App.java
@@ -1,4 +1,4 @@
-package javafx.aula;
+package curso.javafx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-
 
 public class App extends Application {
 
---
+++ JavaFx/Aulas/aula2/src/main/java/curso/javafx/PrimaryController.java
@@ -0,0 +1,11 @@
+package curso.javafx;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+    }
+}
