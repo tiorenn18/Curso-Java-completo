161ebc0260a97c0c891eb80c704515e3871f2885
Tiorenn <ericorenanleal124@gmail.com>
2026-05-19 17:29:51 -0300

    Limitacoes para TextField - Interface initializable

--- JavaFx/Aulas/aula6/src/main/java/cursojava/javafx/SumController.java
+++ JavaFx/Aulas/aula6/src/main/java/cursojava/javafx/SumController.java
@@ -1,14 +1,18 @@
 package cursojava.javafx;
 
+import java.net.URL;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
-import cursojava.javafx.util.Alerts;
+import cursojava.javafx.gui.util.Alerts;
+import cursojava.javafx.gui.util.Constraints;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 
-public class SumController {
+public class SumController implements Initializable{
 
     @FXML
     private TextField num1;
@@ -31,5 +35,13 @@ public class SumController {
         } catch (NumberFormatException e) {
             Alerts.showAlerts("Error", "Parse error", e.getMessage(), AlertType.ERROR);
         }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        Constraints.setTextFieldDouble(num1);
+        Constraints.setTextFieldDouble(num2);
+        Constraints.setTextFieldMaxLength(num1, 12);
+        Constraints.setTextFieldMaxLength(num2, 12);
     }
 }
